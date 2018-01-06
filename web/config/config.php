b77e2468f540b56b7bc773b90078b7f46b89ef67
pphome2 <pphome2@gmail.com>
2018-01-06 14:18:19 +0100

    Bug fix. Improve appearance.
    Signed-off-by: pphome2 <pphome2@gmail.com>
    
    Signed-off-by: pphome2 <pphome2@gmail.com>
    
    Signed-off-by: pphome2 <pphome2@gmail.com>

--- web/config/config.php
+++ web/config/config.php
@@ -11,15 +11,18 @@
 
 $COPYRIGHT="© 2018. <a href=https://github.com/pphome2/docman>DocMan</a>";
 
-# Password1
-$DM_PASS="dc1d82467c3acea982372b8217bc386a";
+# Karakter1
+$DM_PASS="196f9674066aa625b539c2d677d0b676";
 
-$DM_SITENAME="DocMan - Sample";
-$DM_SITE_HOME="http://www.example.com";
+$DM_SITENAME="DocMan - Karakter";
+$DM_SITE_HOME="http://www.karakter.info/v2";
 $DM_DOC_ROOT=".";
 $DM_CONFIG_DIR="config";
 
 $DM_CSS="site.css";
+$DM_CSS2="";
+$DM_JS_BEGIN="";
+$DM_JS_END="js_end.js";
 $DM_HEADER="header.php";
 $DM_FOOTER="footer.php";
 
@@ -47,6 +50,8 @@ $L_FILEDELETE="Fájl törlése";
 
 $L_SECTIONCREATE="Szekció létrehozása";
 $L_SECTIONDELETE="Szekció törlése";
+
+$L_DOCDELETE="Dokumentum törlése";
 
 
 ?>
