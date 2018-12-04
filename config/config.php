51689c8ee4333a6b81031083fd4428bad8398ff5
pphome2 <pphome2@gmail.com>
2018-12-04 18:26:57 +0100

    20181204

---
+++ config/config.php
@@ -0,0 +1,37 @@
+<?php
+
+ #
+ # DocMan - document manager for website
+ #
+ # info: main folder copyright file
+ #
+ #
+
+# configuration - need change it
+
+$COPYRIGHT="© 2018. <a target='_blank' href=https://github.com/pphome2/docman>DocMan</a>";
+
+# password
+$DM_PASS="5f4dcc3b5aa765d61d8327deb882cf99";
+
+$DM_SITENAME="DocMan - Test";
+$DM_SITE_HOME="http://www.example.hu";
+$DM_DOC_ROOT=".";
+$DM_CONFIG_DIR="config";
+
+$DM_CSS="site.css";
+$DM_CSS2="";
+$DM_JS_BEGIN="";
+$DM_JS_END="js_end.js";
+$DM_HEADER="header.php";
+$DM_FOOTER="footer.php";
+
+# language
+
+$DM_LANGFILE="hu.php";
+
+
+# login timeout second
+$LOGIN_TIMEOUT=600;
+
+?>
---
+++ footer.php
@@ -0,0 +1,28 @@
+
+</div>
+
+<footer>
+
+<div class="row">
+  <div class="column3">
+  <div class="columnspace">
+  </div>
+  </div>
+  <div class="column3">
+    <div class="columnspace">
+  </div>
+  </div>
+  <div class="column3">
+    <div class="columnspace">
+  </div>
+  </div>
+</div>
+  <ul class="sidenav"><li><?php echo($COPYRIGHT); ?>	<a href=admin.php>A</a></li></ul></div>
+
+
+</footer>
+
+</div>
+
+</body>
+</html>
