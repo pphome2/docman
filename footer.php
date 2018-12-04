431a4ddd630394b1c69725d7979af6d375afa3d8
pphome2 <pphome2@gmail.com>
2018-12-04 18:39:08 +0100

    20181204

--- footer.php
+++ footer.php
@@ -2,21 +2,6 @@
 </div>
 
 <footer>
-
-<div class="row">
-  <div class="column3">
-  <div class="columnspace">
-  </div>
-  </div>
-  <div class="column3">
-    <div class="columnspace">
-  </div>
-  </div>
-  <div class="column3">
-    <div class="columnspace">
-  </div>
-  </div>
-</div>
   <ul class="sidenav"><li><?php echo($COPYRIGHT); ?>	<a href=admin.php>A</a></li></ul></div>
 
 
