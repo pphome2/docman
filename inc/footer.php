bceb323dba51b892ac979cdffa9c0a72da6aba5f
Péter <pphome2@gmail.com>
2020-09-14 10:11:02 +0200

    Új verzió.

--- inc/footer.php
+++ inc/footer.php
@@ -17,8 +17,8 @@
 		$nextstyle=0;
     }
 
-    if (($MA_ENABLE_COOKIES)and(!$MA_LOGGEDIN)){
-	echo("<p class=padleft>$L_COOKIE_TEXT</p>");
+    if (($MA_ENABLE_COOKIES)and(!$MA_LOGGEDIN)and(!$MA_PRIVACY_PAGE)){
+		echo("<p class=padleft>$L_COOKIE_TEXT <a class=\"privacybutton\" href=\"$MA_PRIVACY\">$L_PRIVACY_MENU</a></p>");
     }
 
 ?>
